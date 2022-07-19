75840efbe9cb9a5ab3343d7b9ca3ebc07dbfc800
kagikn <kagikn.3939@gmail.com>
2022-07-19 22:10:26 +0900

    Debounce search input

--- src/features/search-tracks/routes/SearchView.tsx
+++ src/features/search-tracks/routes/SearchView.tsx
@@ -1,27 +1,36 @@
-import {useCallback, useEffect} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 import {useMatch, useNavigate} from '@tanstack/react-location';
 import {SvgIcon} from '@/components/Elements/SvgIcon';
 import SongSearchResultList from '../components/SongSearchResultList';
 import SearchBarForm from '../components/SearchBarForForm';
 import useSearchInput from '../hooks/useSearchInput';
+import useDebounce from '../hooks/useDebounce';
 
 export const SearchView = (): JSX.Element => {
   const {
     data: {query},
   } = useMatch();
-  const navigate = useNavigate();
+  const [inputtedSearchTerm, setInputtedSearchTerm] = useState<string>(query);
+  const debouncedSearchTerm = useDebounce(inputtedSearchTerm, 400);
+  const isFirstRender = useRef(true);
 
-  const onChangeCallback = useCallback(
-    (e) => {
-      const queryUri = encodeURIComponent(e.target.value.trimEnd());
-      const targetUrl = queryUri !== '' ? `/search/${queryUri}` : '/search';
-      navigate({
-        to: targetUrl,
-        replace: true,
-      });
-    },
-    [navigate]
-  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isFirstRender && isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    const queryUri = encodeURIComponent(debouncedSearchTerm.trimEnd());
+    const targetUrl = queryUri !== '' ? `/search/${queryUri}` : '/search';
+    navigate({
+      to: targetUrl,
+      replace: true,
+    });
+  }, [navigate, debouncedSearchTerm]);
+
+  const onChangeCallback = useCallback((e) => {
+    setInputtedSearchTerm(e.target.value);
+  }, []);
   const onClearCallback = useCallback(() => {
     navigate({
       to: '/search',
@@ -29,13 +38,9 @@ export const SearchView = (): JSX.Element => {
     });
   }, [navigate]);
 
-  const {value, resettedOnLastValueChange, onChange, onClear} = useSearchInput(
-    decodeURIComponent(query as string),
-    onChangeCallback,
-    onClearCallback
-  );
-
-  const queryValue = value.trimEnd();
+  const queryValue = decodeURIComponent(
+    debouncedSearchTerm as string
+  ).trimEnd();
 
   const ListMemorized = <SongSearchResultList query={queryValue} />;
 
@@ -43,9 +48,9 @@ export const SearchView = (): JSX.Element => {
     <>
       <div className="flex items-center justify-center min-w-0 h-16 bg-black-0.85-opacity py-0 px-3 fixed top-0 w-full z-[1] backdrop-blur-md">
         <SearchBarForm
-          value={value}
-          onInputChange={onChange}
-          onInputClearButtonPressed={onClear}
+          value={inputtedSearchTerm}
+          onInputChange={onChangeCallback}
+          onInputClearButtonPressed={onClearCallback}
         />
       </div>
       {ListMemorized}
